17ec1d33c5521b7ce0d3119913e541e1b88ad7d6
noobmaster69jr <srashwin22@gmail.com>
2023-06-13 15:46:29 +0530

    added Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,81 @@
 import {useState} from 'react'
-
+import {DropdownButton, Dropdown} from 'react-bootstrap'
 function Login(){
-    return(<div className ="bg-info d-flex justify-content-center align-items-center vh-100">
-            Login
-    </div>)
+    const [showSignup, setShowSignup] = useState(false)
+    const [userType, setUserType] = useState("CUSTOMER")
+    const toggleSignup = () =>{
+        setShowSignup(!showSignup)
+    }
+    const handleClick = (e) =>{
+        setUserType(e)
+    }
+    return (
+      <div className="bg-info d-flex justify-content-center align-items-center vh-100">
+        <div
+          className="card p-5 rounded-4 shadow-lg"
+          style={{ width: 20 + "rem" }}
+        >
+          <h4 className="text-center text-info">
+            {showSignup ? "Sign Up" : "Log In"}
+          </h4>
+
+          <form>
+            <div className="input-group">
+              <input
+                type="text"
+                className="form-control m-1"
+                placeholder="User id"
+              />
+            </div>
+
+            {showSignup && (
+              <>
+                <div className="input-group">
+                  <input
+                    type="text"
+                    className="form-control m-1"
+                    placeholder="Username"
+                  />
+                </div>
+                <div className="input-group">
+                  <input
+                    type="email"
+                    className="form-control m-1"
+                    placeholder="Email"
+                  />
+                </div>
+                <div className="d-flex justify-content-between m-1">
+                    <span className='my-1'>User Type</span>
+                    <DropdownButton align="end" title={userType} id="userType" variant="light"
+                    onSelect={handleClick}>
+                        <Dropdown.Item eventKey="CUSTOMER"> CUSTOMER</Dropdown.Item>
+                        <Dropdown.Item eventKey="ENGINEER"> ENGINEER</Dropdown.Item>
+                    </DropdownButton>
+                </div>
+
+              </>
+            )}
+            <div className="input-group">
+              <input
+                type="password"
+                className="form-control m-1"
+                placeholder="Password"
+              />
+            </div>
+
+            <div className="input-group">
+              <input
+                type="submit"
+                className="btn btn-info fw-bolder form-control m-1 text-white" value={showSignup ?
+                "Sign Up": "Log In"}/>
+            </div>
+                    <div className='m-1 text-center text-primary' onClick={toggleSignup}>
+                        {showSignup ? "Already have an account? Login": "Don't have an account? Sign up"}
+                    </div>
+          </form>
+        </div>
+      </div>
+    );
 }
 
 export default Login;
